79693b90ffd32fdbd147f6af40c0b5ad485cce66
Sumit Kumar <sumitkumar184280@gmail.com>
2025-11-18 00:09:31 +0530

    solve some responsive issue

--- src/components/homepageComponents/HeroSection.jsx
+++ src/components/homepageComponents/HeroSection.jsx
@@ -3,10 +3,11 @@
 
 export default function HeroSection() {
     return (
-        <section className="w-full bg-[#f2f4f7] flex flex-col justify-center min-h-screen py-4 sm:py-8 md:py-24 text-center px-4">
+        <section className="w-full bg-[#f2f4f7] flex flex-col justify-center md:min-h-screen py-24 sm:py-3 md:py-24 text-center px-4">
+
             <a
                 href="tel:18778431717"
-                className="text-white bg-blue-600 py-3 px-3 sm:px-4 rounded-full mb-4 font-medium text-base block md:hidden mt-2"
+                className="text-white bg-blue-600 py-1.5 px-3 sm:px-4 rounded-full mb-4 font-medium text-base inline-block md:hidden"
             >
                 1-888-812-8678
             </a>
